0bd7f80883c15dc6588bc320d1dbca42acd98644
guadalupeabrile <guadalupeabrile@gmail.com>
2023-09-04 10:20:08 +1000

    react version

--- src/components/Hero/HeroSliderOne.js
+++ src/components/Hero/HeroSliderOne.js
@@ -3,9 +3,10 @@ import TextLoop from "react-text-loop";
 import bgImg from "../../assets/images/background/pagina-bg-1.png"; //tamano imagen 1680x980
 import Scrollspy from "react-scrollspy";
 
+
 const HeroSliderOne = forwardRef((props, ref) => {
 
-  const tags = ["HELLO", "WE'RE", "THE", "PUR", "LIFE"];
+  const tags = ["HELLO", "I'M", "FELICITAS", "RUIZ"];
 
   return (
     <section className="pt-0 pb-0" id="home" ref={ref}>
@@ -17,15 +18,15 @@ const HeroSliderOne = forwardRef((props, ref) => {
         <div className="hero-text-wrap">
           <div className="hero-text white-color">
             <div className="container text-center">
-              <h1 className="white-color font-700">The Pur</h1>
+              <h2 className="white-color font-700">FELICITAS RUIZ</h2>
               <div className="animate-caption">
-                <h2 className="white-color mt-30">
+                <h3 className="white-color mt-30">
                   <TextLoop mask={true}>
                     {tags.map((tag, i) => (
                       <span key={i}>{tag}</span>
                     ))}
                   </TextLoop>
-                </h2>
+                </h3>
               </div>
               <Scrollspy
                 items={["products", "suscribe"]}
