08826f8d7318948af0371756537dd034a7cafe61
guadalupeabrile <guadalupeabrile@gmail.com>
2023-09-08 10:47:31 +1000

    pb Welcome section

--- src/components/WelcomeSection/Welcome.js
+++ src/components/WelcomeSection/Welcome.js
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react';
 import HeadingSection from '../HeadingSection/HeadingSection';
 
 const Welcome = forwardRef(({ title, tagline, children }, ref) => (
-  <section ref={ref} className="first-ico-box pb-10" id="sobreMi">
+  <section ref={ref} className="first-ico-box" id="sobreMi">
     <div className="container">
       <div className="row">
         <HeadingSection title={title} tagline={tagline}>
